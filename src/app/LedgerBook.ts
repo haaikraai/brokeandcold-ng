03e669911f78c0cdb1e06a8c46102c67dbc547ae
Andre <haaikraai@gmail.com>
2025-03-08 15:41:18 +0200

    Merged with remote

--- src/app/LedgerBook.ts
+++ src/app/LedgerBook.ts
@@ -30,7 +30,6 @@ export class LedgerBook {
     this.ledgerData.push(realEntry);
   }
 
-<<<<<<< HEAD
   /**
    * Updates an existing item in the ledger with new data.
    *
@@ -43,24 +42,17 @@ export class LedgerBook {
    * @returns The difference in 'amount' between the old and new entries. This can be useful to change the summed total balance as well
    */
   updateItem(id: number, entry: Transaction): number {
-=======
-  updateItem(id: number, entry: Transaction) {
->>>>>>> 05e3df9d9d96f0b2d411035bca8304ef7e801caf
     const realEntry: OmniscientTransaction = {
       id: id,
       ...entry
     };
     const index = this.ledgerData.findIndex(item => item.id == id);
-<<<<<<< HEAD
     const amountChange = realEntry.amount - this.ledgerData[index].amount;
 
     this.ledgerData[index] = JSON.parse(JSON.stringify(realEntry)) as OmniscientTransaction; //realEntry;
     this.saveHistory();
 
     return amountChange
-=======
-    this.ledgerData[index] = JSON.parse(JSON.stringify(realEntry)) as OmniscientTransaction; //realEntry;
->>>>>>> 05e3df9d9d96f0b2d411035bca8304ef7e801caf
   }
 
 
--- src/app/settingspage/settingspage.component.ts
+++ src/app/settingspage/settingspage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BeanService } from 'bean-service.service.ts';
 
 @Component({
   selector: 'app-settingspage',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './settingspage.component.css'
 })
 export class SettingspageComponent {
+    private beanService = inject(BeanService);
+    settingsData = this.beanService.balanceData;
 
+    constructor() {}
+
+    OnInit() {
+        console.log('opening settings. data is:');
+        console.log(this.settingsData);
+        }
 }
